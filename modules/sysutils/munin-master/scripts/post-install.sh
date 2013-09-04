72f5f0dd7c99c90cecafaf7f3f1f464618368b82
Ken Moore <ken@pcbsd.org>
2013-09-04 10:36:45 -0400

    Fix up the sysutils/munin-master module - it needs shells/bash for the scripts

--- modules/sysutils/munin-master/scripts/post-install.sh
+++ modules/sysutils/munin-master/scripts/post-install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-sed -i.bak "s,${PBI_PROGDIRPATH}/bin/bash,/bin/bash,g" ${PBI_PROGDIRPATH}/bin/munin-check
 sed -i.bak "s,/usr/bin/perl,${PBI_PROGDIRPATH}/bin/perl,g" ${PBI_PROGDIRPATH}/share/munin/munin-*
 
 #Make the log file directory
