0617f0c7716a0c380551d7fc51b0ac97b7883725
Ken Moore <ken@pcbsd.org>
2013-11-11 15:05:59 -0500

    Get the emulators/i386-wine-devel PBI module cleaned up. Add info output to the port-install.sh script, and require root permissions to install the PBI (needed for the patch-nvidia.sh script that comes with the port). Also trigger a rebuild

--- modules/emulators/i386-wine-devel/scripts/post-install.sh
+++ modules/emulators/i386-wine-devel/scripts/post-install.sh
@@ -2,4 +2,6 @@
 PREFIX=/usr/pbi/wine-amd64
 
 # Run the nvidia setup / detection script
+echo "Running patch-nvidia.sh"
 sh ${PREFIX}/share/wine/patch-nvidia.sh
+echo "Done with post-install script"
