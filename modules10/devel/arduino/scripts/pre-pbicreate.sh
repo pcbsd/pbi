95253a01681c9c28ca91e7bcdbace653d637da01
Ken Moore <ken@pcbsd.org>
2013-12-02 10:45:57 -0500

    Fix up the devel/arduino module. Remove all the uarduno driver stuff, this can be done on the base system with packages instead - the PBI is just for the software

--- modules10/devel/arduino/scripts/pre-pbicreate.sh
+++ modules10/devel/arduino/scripts/pre-pbicreate.sh
@@ -9,9 +9,3 @@ sed -i.bak "s,serial.port=COM1,serial.port=cuaU0,g" ${PREFS_FILE}
 
 #setup the web-browser launcher
 echo "launcher=/usr/local/bin/openwith" >> ${PREFS_FILE}
-
-#Now make sure the uarduno kernel module is copied into the PBI
-mkdir ${PBI_PROGDIRPATH}/kmod
-KMOD=`find /boot | grep -v kernel | grep uarduno.ko`
-echo "Found uarduno.ko: ${KMOD}"
-cp ${KMOD} ${PBI_PROGDIRPATH}/kmod/uarduno.ko
