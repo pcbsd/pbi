23bc8791b203e4e213fcb4d6783164361f65ff39
Ken Moore <ken@pcbsd.org>
2014-01-07 16:52:11 -0500

    Remove the 9.x emulators/i386-wine-devel module, and trigger rebuilds for the wine64 and wine64-devel modules.

--- modules/emulators/i386-wine-devel/scripts/post-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-PREFIX=/usr/pbi/wine-amd64
-
-# Run the nvidia setup / detection script
-echo "Running patch-nvidia.sh"
-sh ${PREFIX}/share/wine/patch-nvidia.sh
-echo "Done with post-install script"
