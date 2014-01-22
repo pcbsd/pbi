2ec4cb7fa44ba86f17e3928856ba4b5c0732fef1
Ken Moore <ken@pcbsd.org>
2014-01-22 10:40:08 -0500

    Remove my custom Minecraft "nonport". This has been replaced by the offical games/minecraft-client module for 10.x

--- modules10/nonport/minecraft/scripts/post-portmake.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#fetch the minecraft.jar(s) from the main website
-cd ${PBI_PROGDIRPATH}
-
-fetch https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft.jar
-fetch https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar
-
