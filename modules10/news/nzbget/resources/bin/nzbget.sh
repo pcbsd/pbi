273085476a47c4d992e176211709eb57d63cd726
Kris Moore <kris@pcbsd.org>
2014-01-22 10:09:56 -0500

    Fix nzbget, needs 755 on target binary

--- modules10/news/nzbget/resources/bin/nzbget.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PROGDIR=/usr/local
-
-if [ ! -f ~/.nzbget ]; then
-  #Copy the sample configuration file into the user's dir
-  cp ${PROGDIR}/etc/nzbget.conf.sample ~/.nzbget
-fi
-#Start the main program
-${PROGDIR}/bin/nzbget $@
